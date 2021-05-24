6ec971c77240c9ce4aad17689fd3b3248222ef44
mofumofut <mofumofu150416@gmail.com>
2021-05-24 18:25:04 +0900

    Add create User function & User SSL and the Puma webserverin Production

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,7 +4,7 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com", password: "password", password_confirmation: "password")
   end
 
   test "should be valid" do
